ca58e80fc168b2e9d0967945cc5c8a0f958f939d
Savaliya8716 <jaylimbasiya34@gmail.com>
2022-11-21 10:49:21 +0530

    c_languge

---
+++ FACT_ch_2.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+
+
+main()
+{
+	  clrscr();
+	  printf("name\t\t:jay limbasiya\n");
+	  printf("Age\t\t:18\n");
+	  printf("contects\t:8905606789\n");
+	  getch();
+
+
+
+}
+
+
---
+++ fact_ch_2 (3).C
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+{
+	clrscr();
+	printf("- - - - -n");
+	printf("|\t|\n");
+	printf("f\t|\n");
+	printf("a\t|\n");
+	printf("c\t|\n");
+	printf("t\t|\n");
+	printf("|\t|\n");
+	printf("- - - - -\n");
+	getch();
+
+
+
+}
